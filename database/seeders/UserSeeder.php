4c7ecb95198204ad2709685d7081182f82e1ae5c
GG-G-M <g.mantilla.545557@umindanao.edu.ph>
2025-10-07 22:19:07 +0800

    Seeder Fixed

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -6,24 +6,24 @@ use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 
-class AdminUserSeeder extends Seeder
+class UserSeeder extends Seeder
 {
     public function run()
     {
         User::create([
-            'name' => 'Admin User',
-            'email' => 'admin@dimdi.com',
+            'name' => 'Gilgre Gene G. Mantilla',
+            'email' => 'gilgregenemantilla@gmail.com',
             'password' => Hash::make('12345678'),
-            'role' => 'admin',
+            'role' => 'customer',
             'phone' => '123-456-7890',
-            'address' => 'Admin Address',
-            'city' => 'Admin City',
-            'state' => 'Admin State',
+            'address' => 'Bankal Skibidi',
+            'city' => 'Davao City',
+            'state' => 'Davao State',
             'zip_code' => '12345',
-            'country' => 'Admin Country'
+            'country' => 'Philippines'
         ]);
 
-        $this->command->info('Admin user created successfully!');
+        $this->command->info('Customer created successfully!');
         $this->command->info('Email: admin@gmail.com');
         $this->command->info('Password: 12345678');
     }
