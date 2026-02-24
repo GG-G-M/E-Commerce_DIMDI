4d651757d78742ec26ac2fd83681abf380959257
GG-G-M <g.mantilla.545557@umindanao.edu.ph>
2026-02-24 23:05:55 +0800

    fix

--- database/seeders/SuperAdminSeeder.php
+++ database/seeders/SuperAdminSeeder.php
@@ -10,20 +10,16 @@ class SuperAdminSeeder extends Seeder
 {
     public function run()
     {
-        // Check if super admin already exists
-        if (!User::where('email', 'superadmin@system.com')->exists()) {
-            User::create([
+        User::updateOrCreate(
+            ['email' => 'superadmin@gmail.com'],
+            [
                 'first_name' => 'Super',
                 'last_name' => 'Admin',
-                'email' => 'superadmin@gmail.com',
-                'password' => Hash::make('12345678'), 
-                'role' => User::ROLE_SUPER_ADMIN,
+                'password' => Hash::make('12345678'),
+                'role' => 'super_admin',
                 'phone' => '0000000000',
-                'is_active' => true,
-            ]);
-            $this->command->info('Super admin account created!');
-            $this->command->info('Email: superadmin@gmail.com');
-            $this->command->info('Password: 12345678');
-        }
+                'is_active' => 1
+            ]
+        );
     }
 }
